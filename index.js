0f25b9f460561e0d472719498c3d34fa87b8c080
Huy Cao <hieppro138@gmail.com>
2025-05-19 13:05:19 +0700

    Update

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ const server = net.createServer((socket) => {
     'Content-Type: text/event-stream',
     'Connection: Keep-Alive',
     'Cache-Control: no-cache',
-    'Some-Custom-Header: test',
+    'Some-Custom-Header: Custom 1',
+    'X-Custom-Header: Custom 2',
+    '', // <== This empty string adds the required \r\n
+    ''  // <== Ensures final output ends with \r\n\r\n
   ].join('\r\n');
 
-  socket.write(headers);
+  socket.write(headers); // Headers + \r\n\r\n now correctly sent
 
   let counter = 0;
   const intervalId = setInterval(() => {
-    socket.write(`data: Event ${counter++}\r\n\r\n`);
+    const chunk = `data: Event ${counter++}\n\n`;
+    socket.write(chunk);
   }, 1000);
 
   socket.on('close', () => clearInterval(intervalId));
@@ -25,32 +29,3 @@ const server = net.createServer((socket) => {
 server.listen(8081, () => {
   console.log('SSE server running on port 8081');
 });
-
-
-function startClient(targetUrl) {
-    const proxy = { hostname:'192.168.0.9', port: 9091 };
-
-    const options = {
-        protocol: 'http:',
-        hostname: proxy.hostname,
-        port: proxy.port,
-        path: targetUrl, 
-        headers: {
-            'Host': 'localhost:8081',
-            'Connection': 'Keep-Alive',
-            'Accept': 'text/event-stream',
-            'Accept-Encoding': 'gzip',
-            'User-Agent': 'okhttp/4.12.0'
-        }
-    };
-
-    const req = http.request(options, (res) => {
-        Object.entries(res.headers).forEach(([key, value]) => {
-            console.log(`${key}: ${value}`);
-        });
-    });
-
-    req.end();
-}
-
-startClient('http://localhost:8081/events');
